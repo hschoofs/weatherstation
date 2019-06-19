f21e7750fba8e70da39fd8457d5dae2638fac4fa
Schoofs <HSC@staff.hsrw>
2019-06-19 11:37:12 +0200

    changes threading

--- logger/datalog.py
+++ logger/datalog.py
@@ -1,23 +1,27 @@
 import psycopg2
 import time
 import serial
+import threading
+import schedule
 from datetime import datetime as dt
 import logging
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s:%(levelname)s:%(message)s", handlers=[logging.FileHandler("combi.log")])  ##logger 
-ser = serial.Serial('/dev/ttyACM0', baudrate=19200, timeout=1)      ##serial configuration
+logging.basicConfig(level=logging.INFO, format="%(asctime)s:%(levelname)s:%(message)s", handlers=[logging.FileHandler("combi.log"),logging.StreamHandler()])  #logger
+ser = serial.Serial('/dev/ttyACM0', baudrate=19200, timeout=1)      #serial configuration
 data = []
-timer = 0
+# timer = 0
+state = 0
 
 
 
-def insert_values(column, value):
+def insert_values(value):
         table = "sensordata"
-        sql = "INSERT INTO {} ({}) VALUES ({});".format(table, column, value)       ##sql query including columns and data
+        column = "wind_speed, wind_direction, air_temperature, air_relhumidity, smp10, pqsl, soil_moisture, soil_tempblue, soil_tempred, air_pressure, precipitation, created_at"
+        sql = "INSERT INTO {} ({}) VALUES ({});".format(table, column, value)       #sql query including columns and data
         conn = None
         try:
             conn = psycopg2.connect(user = "pi",
-                                    password = "*********",
+                                    password = "pwd123",
                                     host = "10.180.12.123",
                                     port = "5432",
                                     database = "test")
@@ -27,15 +31,17 @@ def insert_values(column, value):
             cursor.close()
         except (Exception, psycopg2.DatabaseError) as error:
             logging.info(error)
-            f = open("not_uploaded.txt", "a")       ##save data to file in case an error occurs and data cannot be uploaded to database
+            f = open("not_uploaded.txt", "a")       #save data to file in case an error occurs and data cannot be uploaded to database
             f.write(value+'\n')
             f.close
+            global state
+            state = 0
         finally:
             if conn is not None:
                 conn.close()
 
 
-def readser():          ##reading serial response from datalogger after sending ascii telegram
+def readser():          #reading serial response from datalogger after sending ascii telegram
     eol = b'\r'
     leneol = len(eol)
     line = bytearray()
@@ -51,10 +57,10 @@ def readser():          ##reading serial response from datalogger after sending 
 
 
 def readdata(req):
-    ser.write(req)      ##send telegram to datalogger
+    ser.write(req)      #send telegram to datalogger
     res=readser()
     tmp=res.decode()
-    return tmp[1:-1]        ##return formatted response
+    return tmp[1:-1]        #return formatted response
 
 
 def sync_time():
@@ -62,15 +68,15 @@ def sync_time():
     tl = dt.strftime(t, "%y%m%d%H%M%S")
     time_str = '$01G'+tl+'\r'
     time_bytes = time_str.encode('utf-8')
-    set_time = readdata(time_bytes)         ##format time and synchronize with datalogger
+    set_time = readdata(time_bytes)         #format time and synchronize with datalogger
     logging.info('time set to:')
     logging.info(tl)
 
 
 def channel():
-    global timer
+    # global timer
     data.clear()
-    wind_speed = readdata(b'$01R01\r')          ##ascii telegram for each sensor on datalogger
+    wind_speed = readdata(b'$01R01\r')          #ascii telegram for each sensor on datalogger
     wind_direction = readdata(b'$01R02\r')
     air_temperature = readdata(b'$01R03\r')
     air_relhumidity = readdata(b'$01R04\r')
@@ -83,7 +89,7 @@ def channel():
     precipitation = readdata(b'$01R0B\r')
     readtime = readdata(b'$01H\r')
 
-    time = dt.strptime(readtime, "%d%m%y%H%M%S")        ##format time read from datalogger
+    time = dt.strptime(readtime, "%d%m%y%H%M%S")        #format time read from datalogger
     str_time = dt.strftime(time,  "%d%m%y %H%M%S")
 
     data.append(wind_speed)
@@ -97,23 +103,64 @@ def channel():
     data.append(soil_tempred)
     data.append(air_pressure)
     data.append(precipitation)
-    data.append(str_time)           ##insert sensor values into list
+    data.append(str_time)           #insert sensor values into list
 
-    col = "wind_speed, wind_direction, air_temperature, air_relhumidity, smp10, pqsl, soil_moisture, soil_tempblue, soil_tempred, air_pressure, precipitation, created_at"          #collumns in database
-    val = str(data)[1:-1]           ##convert list to string and remove brackets
-    #logging.info(val)
-    insert_values(col, val)         ##insert values into database
-    timer += 1                      ##counter for synchronizing time
+    # col = "wind_speed, wind_direction, air_temperature, air_relhumidity, smp10, pqsl, soil_moisture, soil_tempblue, soil_tempred, air_pressure, precipitation, created_at"          #collumns in database
+    val = str(data)[1:-1]           #convert list to string and remove brackets
+    logging.info(val)
+    insert_values(val)         #insert values into database
+    # timer += 1                      #counter for synchronizing time
 
 
-while True:
+def run_threaded(fkt):
+    reuploading_thread = threading.Thread(target=fkt)
+    reuploading_thread.start()
+
+
+def reupload_data():
+    sync_time()
+    global state
+    counter = 0
     try:
-        channel()
-        if timer == 8640:               ##sync time every 8640 iterations ~ 24 hours
-            sync_time()
-            timer = 0
+        not_uploaded = open("not_uploaded.txt", "r+")
+        not_uploaded_copy = open("not_uploaded_copy.txt", "a")
+        for i in not_uploaded:
+            if state is not 0:
+                insert_values(i)
+                counter += 1
+        if counter > 1:
+            not_uploaded.seek(0)
+            for i in not_uploaded:
+                not_uploaded_copy.write(i)
+            not_uploaded.seek(0)
+            not_uploaded.truncate()
+            logging.info("not_uploaded deleted")
+        not_uploaded.close()
+        not_uploaded_copy.close()
+        state = 1
     except:
-        logging.info("error funct. channel")
-        time.sleep(50)                  ##sleep if data cannot be read from datalogger
-    finally:
-        time.sleep(10)					##sleep for 10 seconds
+        logging.info("error while reuploading")
+        state = 1
+
+
+
+
+if __name__ == "__main__":
+    schedule.every(10).seconds.do(channel)
+    schedule.every().week.do(run_threaded, reupload_data)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+#
+# while True:
+#     try:
+#         channel()
+#         if timer == 8600:               #sync time every 8600 iterations ~ 24 hours
+#             sync_time()
+#             timer = 0
+#     except:
+#         logging.info("error funct. channel")
+#         time.sleep(50)                  #sleep if data cannot be read from datalogger
+#     finally:
+#         time.sleep(10)
